a45ea98c8222c0e9d67ad193d4742c4a4ef62231
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-11-02 06:02:56 +0100

    refactor: make Handshake inherit from BasePacket and register in PacketRegistry

--- protocol/handshake.py
+++ protocol/handshake.py
@@ -1,9 +1,14 @@
 # protocol/handshake.py
 
-from network.utils import write_varint, write_string, read_varint, read_string
 import struct
 
-class Handshake:
+from network.utils import write_varint, write_string
+from protocol.registry import PacketRegistry
+from protocol.base import BasePacket
+
+
+@PacketRegistry.register
+class Handshake(BasePacket):
     PACKET_ID = 0x00
 
     def __init__(self, protocol_version: int, server_address: str, server_port: int, next_state: int):
@@ -21,14 +26,3 @@ class Handshake:
         data.extend(write_varint(self.next_state))
         full_payload = bytes(data)
         return write_varint(len(full_payload)) + full_payload
-
-    def deserialize(self, data: bytes):
-        offset = 0
-        packet_id, offset = read_varint(data, offset)
-        if packet_id != self.PACKET_ID:
-            raise ValueError(f"Invalid Packet ID: expected {self.PACKET_ID}, got {packet_id}")
-        self.protocol_version, offset = read_varint(data, offset)
-        self.server_address, offset = read_string(data, offset)
-        self.server_port = struct.unpack_from('>H', data, offset)[0]
-        offset += 2
-        self.next_state, offset = read_varint(data, offset)    
