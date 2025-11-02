ff2e74092c67494c0872daf5b0779624fd55face
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-11-02 15:34:09 +0100

    protocol: add StatusResponse + deserialize for StatusRequest

--- protocol/status.py
+++ protocol/status.py
@@ -2,7 +2,7 @@
 
 from protocol.base import BasePacket
 from protocol import PacketRegistry, State, Direction
-from network.utils import write_varint
+from network.utils import write_varint, read_string, write_string
 
 
 @PacketRegistry.register
@@ -12,5 +12,28 @@ class StatusRequest(BasePacket):
     DIRECTION = Direction.SERVERBOUND
 
     def serialize(self) -> bytes:
-        data = write_varint(self.PACKET_ID)
-        return write_varint(len(data)) + data
+        body = write_varint(self.PACKET_ID)
+        return write_varint(len(body)) + body
+
+    @classmethod
+    def deserialize(cls, data: bytes, offset: int = 0):
+        return cls(), offset
+
+
+@PacketRegistry.register
+class StatusResponse(BasePacket):
+    PACKET_ID = 0x00
+    STATE = State.STATUS
+    DIRECTION = Direction.CLIENTBOUND
+
+    def __init__(self, json_response: str):
+        self.json_response = json_response
+
+    def serialize(self) -> bytes:
+        body = write_varint(self.PACKET_ID) + write_string(self.json_response)
+        return write_varint(len(body)) + body
+
+    @classmethod
+    def deserialize(cls, data: bytes, offset: int = 0):
+        json_response, offset = read_string(data, offset)
+        return cls(json_response), offset
